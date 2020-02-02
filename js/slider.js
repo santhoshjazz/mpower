a4f4aaed15b89495f74757c03d93c157451a05c4
santhoshjazz <60568604+santhoshjazz@users.noreply.github.com>
2020-02-02 10:03:11 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($) {
         responsive: [{
           breakpoint: 767,
           settings: {
-            slidesToShow: 2,
+            slidesToShow: 3,
             slidesToScroll: 1
           }
         },
